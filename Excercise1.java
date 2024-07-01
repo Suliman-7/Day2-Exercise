25791f8ea27198564b7e6cd3084c4ef6180a8a43
Suliman Alodaib <107861497+Suliman-7@users.noreply.github.com>
2024-07-01 19:58:52 +0300

    Add files via upload

---
+++ Excercise1.java
@@ -0,0 +1,140 @@
+import java.util.Scanner;
+
+public class Excercise1 {
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+
+
+
+        // Q1
+
+
+//        System.out.println("Please enter your weight: ");
+//
+//                    double weight = input.nextDouble() ;
+//
+//        System.out.println("Please enter your height: ");
+//
+//                    double height = input.nextDouble() ;
+//
+//                    double bmi = weight / (height * height);
+//
+//                    System.out.println(bmi);
+//
+//    // Q2
+//        System.out.println("Please enter your obtained mark : ");
+//
+//                   double obtained_mark = input.nextDouble() ;
+//                   double Total_mark = 100;
+//
+//                   double Percentage = obtained_mark / Total_mark * 100 ;
+//
+//                   System.out.println("Percentage = " + Percentage + "%");
+//
+//    // Q3
+//
+
+
+//        System.out.println("Please enter currency to exchange : ");
+//
+//                  String currency = input.nextLine();
+//
+//        System.out.println("Please enter amount of original currency  : ");
+//
+//                  double amount = input.nextDouble() ;
+//
+//        System.out.println("please enter exchange rate : ");
+//                  double exchange_Rate = input.nextDouble() ;
+//
+//                  double new_amount = amount * exchange_Rate ;
+//
+//                  System.out.println("Amount in " + currency + " " + new_amount);
+//
+//
+//    // Q4
+//        System.out.println("Please enter sentence : ");
+//
+//                 String sentence = input.nextLine();
+//
+//                 int len = sentence.length();
+//
+//                 StringBuilder rev = new StringBuilder(sentence);
+//
+//                 rev.reverse();
+//
+//                 System.out.println("Length of the string: " + len + " And Reversed string: " + rev);
+//
+//
+//    // Q5
+//
+//        System.out.println("Please enter sentence : ");
+//
+//                 String sentence1 = input.nextLine();
+//
+//                 int startIndex = input.nextInt() ;
+//
+//                 int endIndex = input.nextInt() ;
+//
+//
+//
+//                 System.out.println(sentence1.substring(startIndex,endIndex));
+//
+//
+//
+//    //  Q6
+//
+//        System.out.println("Please enter sentence : ");
+//
+//                  String sentence2 = input.nextLine();
+//
+//                  String keyword = input.nextLine();
+//
+//                  boolean in = sentence2.contains(keyword);
+//
+//                  System.out.println( in ) ;
+//
+//                  if (in == true){
+//                      System.out.println( "Keyword " + keyword + " is present in the sentence" ) ;
+//                  }
+//                  else{
+//                      System.out.println( "Keyword " + keyword + " is not present in the sentence" ) ;
+//                  }
+//
+        // Q7
+
+//        System.out.println("Please enter sentence : ");
+//
+//                  String sentence3 = input.nextLine() ;
+//
+//        System.out.println("Please enter word you want to replace : ");
+//
+//                  String origin = input.nextLine() ;
+//
+//        System.out.println("Please enter new word : ");
+//
+//                  String replacementWord = input.nextLine() ;
+//
+//        System.out.println(sentence3.replace(origin,replacementWord));
+
+
+
+//    //   Q8
+//
+//        System.out.println("Please enter String 1 : ");
+//
+//                 String s1 = input.next() ;
+//
+//        System.out.println("Please enter String 1 : ");
+//
+//                 String s2 = input.next() ;
+//
+//        System.out.println(s1.equalsIgnoreCase(s2));
+
+
+
+
+    }}
+
+
